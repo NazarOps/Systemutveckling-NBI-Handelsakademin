394c388d58bc26d51a5269fdb0eebb459e7ec59b
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-08 17:15:39 +0200

    my first application using class "cat"

---
+++ Monday/First Console Application/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Cat
+{
+    private string name = "Fluffy boi";
+    private string word = "Meooow!";
+
+    static void Main(string[] args)
+    {
+        Cat myCat = new Cat();
+        Console.WriteLine(myCat.name + " says " + myCat.word);
+    }
+}
