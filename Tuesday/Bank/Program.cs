7834686e32a7fb3efef4b47fddb8c691a7fc0fc0
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-09 15:17:22 +0200

    Added a new writeline saying "How is your day?"

---
+++ Tuesday/Bank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bank
+{
+    public class Bank
+    {
+        string BankAccount;
+        string User;
+        string Admin;
+        int Money;
+
+        public void Start()
+        {
+            Console.WriteLine("Welcome to bank");
+            Console.WriteLine("How is your day?");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank account = new Bank();
+            account.Start();
+        }
+    }
+}
+
---
+++ Tuesday/C# Code Tuesday/Program.cs
@@ -0,0 +1,30 @@
+﻿using C__Code_Tuesday;
+using System;
+
+namespace C__Code_Tuesday
+{
+    public class Bank
+    {
+        string Bankaccount;
+
+        string User;
+
+        string Admin;
+
+        int Money;
+
+        public void Start()
+        {
+            Console.WriteLine("Welcome to bank");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank account = new Bank();
+            account.Start();
+        }
+    }
+}
