5ca1a084a167df42a816fd9e214cac02fc96a6a7
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-09 10:26:47 +0200

    new bank application

---
+++ Tuesday/Bank Application/bank program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bank Application
+{
+    public class Bank
+    {
+        string BankAccount;
+        string User;
+        string Admin;
+        int Money;
+
+        public void Start()
+        {
+            Console.WriteLine("Welcome to bank");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank account = new Bank();
+            account.Start();
+        }
+    }
+}
+
