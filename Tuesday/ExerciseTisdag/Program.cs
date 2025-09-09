e138a98626eb145d3ca8bb9bcef4e51e4f95ee82
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-09 15:46:56 +0200

    Class and object example

---
+++ Tuesday/ExerciseTisdag/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ExerciseTisdag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Här skapar jag ett objekt av typ spelare
+            Spelare spelare1 = new Spelare();
+            
+            spelare1.Namn = "Nazar";
+            
+            spelare1.NummerPåTröjan = "1";
+
+            spelare1.Springa();
+
+            Console.WriteLine(spelare1.Namn);
+        }
+    }
+
+    // Här skapar jag en ny klass
+    public class Spelare
+    {
+        // Varje klass består av Attributer och Metoder
+
+        // Attributer - Egenskaper
+        public string Namn;
+
+        public string NummerPåTröjan;
+
+        // Metoder - Vad kan denna klassen göra
+        public void Springa()
+        {
+            Console.WriteLine("Spelare springer");
+        }
+
+        
+    }
+}
