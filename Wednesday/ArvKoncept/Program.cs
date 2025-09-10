1c102f076e98ef71510d89dfd9e4b4aea966ae2e
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-10 20:33:15 +0200

    Added "arv koncept" exercise
    This exercise is about how to inheret properties from a Class called animal and create an object based on the class

---
+++ Wednesday/ArvKoncept/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace ArvKoncept
+{
+
+    public class Cat : Animal
+    {
+        static void Main(string[] args)
+        {
+            Cat myCat = new Cat();
+            myCat.ShowInfo();
+        }
+    }
+}
