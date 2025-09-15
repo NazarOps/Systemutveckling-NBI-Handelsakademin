5b2dcdca3f8c5129ac28580a69af7874409d4d8d
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-15 11:58:29 +0200

    first exercise for 20250915

---
+++ Wednesday/OOP Grund/ArvKoncept/inheret from this class.cs
@@ -0,0 +1,18 @@
+﻿namespace ArvKoncept
+{
+    internal class inheret_from_this_class
+    {
+    }
+
+    public class Animal
+    {
+        int Legs = 4;
+        string Color = "Orange";
+        string Size = "Big";
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Legs: {Legs}, Color: {Color}, Size: {Size}");
+        }
+    }
+}
