3606054cc225648399088bf2ffbfd19a602ba04f
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-19 15:01:02 +0200

    Moved from Monday to Friday folder

---
+++ Friday/OOP Grund/Inlämningsuppgift 2/Person.cs
@@ -0,0 +1,43 @@
+﻿namespace Inlämningsuppgift_2
+{
+    public class Person
+    {
+        private string Name;
+        private int Age;
+        private string Gender;
+
+        public Person(string namn) 
+        {
+            Name = namn;
+        }
+        public string Namn
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public Person(int age)
+        {
+            Age = age;
+        }
+        public int Ålder
+        {
+            get { return Age; }
+            set { Age = value; }
+        }
+        public string Kön
+        {
+            get { return Gender; }
+            set { Gender = value; }
+        }
+
+        public void InfoAboutPerson()
+        {
+            Console.WriteLine($"This persons name is {Namn}, person is {Ålder} years old and persons gender is {Kön}");
+        }
+
+        public Person()
+        { 
+        }
+    }
+}
---
+++ Friday/OOP Grund/Inlämningsuppgift 2/Squat racks.cs
@@ -0,0 +1,10 @@
+﻿namespace Inlämningsuppgift_2
+{
+    public class Squat_racks : Equipment
+    {
+        public void InUse()
+        {
+            Console.WriteLine($"This squat rack is currently not in use. The brand is {Brand}, there are {AmountofEquipment} and most of them are {Condition}\n");
+        }
+    }
+}
