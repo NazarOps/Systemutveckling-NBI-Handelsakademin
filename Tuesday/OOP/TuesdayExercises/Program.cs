810a17436a1d7af2f8e46f84355fcad869f62aab
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-23 13:31:23 +0200

    Added a writeline asking for user name and age and then convert age into int and calculate what year user were born

--- Tuesday/OOP/TuesdayExercises/Program.cs
+++ Tuesday/OOP/TuesdayExercises/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, what's your name?");
+string name = Console.ReadLine();
+
+Console.WriteLine("How old are you?");
+string age = Console.ReadLine();
+int userAge = Convert.ToInt32(age);
+const int currentYear = 2025;
+int birthYear = currentYear - userAge;
+
+Console.WriteLine($"Hello {name}, you were born year {birthYear}.\nHope you will have a nice Tuesday!");
+
