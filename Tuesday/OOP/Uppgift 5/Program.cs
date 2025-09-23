0cbbba49a6936cc9d5fa3bd8b8152b33149cebcb
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-23 16:12:11 +0200

    exercise 5, practicing how to change text color in console and reset

---
+++ Tuesday/OOP/Uppgift 5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+string text = Console.ReadLine();
+Console.WriteLine(text);
+
+Console.ForegroundColor = ConsoleColor.Red;
+string textTwo = Console.ReadLine();
+Console.WriteLine(text + " " + textTwo);
+Console.ResetColor();
