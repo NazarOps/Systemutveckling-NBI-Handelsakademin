e4b09f8d99ae498e4a773a5d899a9c608900c319
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-23 13:58:27 +0200

    uppgift 3 done

---
+++ Tuesday/OOP/Uppgift 3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Type in an integer");
+string Number = Console.ReadLine();
+int Number2 = Convert.ToInt32(Number);
+
+Console.WriteLine("Type in a float");
+string decimaltal = Console.ReadLine();
+float decimalTal = Convert.ToSingle(decimaltal);
+
+Console.WriteLine("Type in a boolean value");
+string boolean = Console.ReadLine();
+bool booleanValue = Convert.ToBoolean(boolean);
+
+Console.WriteLine($"You typed in {Number}, {decimaltal}, {booleanValue}");
