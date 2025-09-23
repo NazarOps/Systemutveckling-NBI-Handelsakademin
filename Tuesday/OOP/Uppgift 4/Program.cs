f8652bf3164cc970877261d8cd829121ffd4c13c
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-23 14:10:18 +0200

    made a small calculator

---
+++ Tuesday/OOP/Uppgift 4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, please type in a number");
+string number = Console.ReadLine();
+
+Console.WriteLine("Please type in another number");
+string numberTwo = Console.ReadLine();
+
+int sum = Convert.ToInt32(numberTwo) + Convert.ToInt32(number);
+int product = Convert.ToInt32(numberTwo) * Convert.ToInt32(number);
+Console.WriteLine($"\nYour total sum is {sum}");
+Console.WriteLine($"Your product is {product}\n");
