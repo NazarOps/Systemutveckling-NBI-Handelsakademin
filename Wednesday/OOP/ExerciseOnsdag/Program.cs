e8697000f6e3099f71598c0eeb6038f13aa335e6
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-24 13:06:44 +0200

    clean code exempel

---
+++ Wednesday/OOP/ExerciseOnsdag/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ExerciseOnsdag
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Här inne skapar vi objekt med mindre rad kod
+
+            //Bil nummer 1
+            Car carOne = new Car();
+            carOne.collectInfo();
+            carOne.printInfo();
+
+
+            // Bil nummer 2
+            Car carTwo = new Car();
+            carTwo.collectInfo();
+            carTwo.printInfo();
+
+
+        }
+    }
+}
---
+++ Wednesday/OOP/ExerciseOnsdag/car.cs
@@ -0,0 +1,33 @@
+﻿namespace ExerciseOnsdag
+{
+    public class Car
+    {
+        // Vi skapar en klass som heter Car
+        public string Brand;
+        public string Model;
+        public int ModelNumber;
+
+        // Vi samlar in information med metoden collectInfo()
+        public void collectInfo()
+        {
+            // Clean Code
+            // Här behöver vi inte skriva Car.brand osv för att vi har redan deklarerat
+            // Vi tar in användarinput som information för brand, model och modelnumber
+
+            Console.WriteLine("\nPlease provide a car brand");
+            Brand = Console.ReadLine();
+
+            Console.WriteLine("Please provide a car model");
+            Model = Console.ReadLine();
+
+            Console.WriteLine("Please provide a modellnumber");
+            ModelNumber = Convert.ToInt32(Console.ReadLine());
+        }   
+
+        public void printInfo()
+        {
+            // Här inne printar vi ut information som vanligt
+            Console.WriteLine($"This car is a {Brand}, the model is {Model} and the number is {ModelNumber}");
+        }   
+    }
+}
