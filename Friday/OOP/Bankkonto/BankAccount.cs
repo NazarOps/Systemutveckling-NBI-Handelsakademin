37fb1f80b92bdd0b354bd40f713e58368b38e1af
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-26 23:38:48 +0200

    made a simple bank application using conditional statements

--- Friday/OOP/Bankkonto/BankAccount.cs
+++ Friday/OOP/Bankkonto/BankAccount.cs
@@ -1,28 +1,44 @@
-﻿using System.Reflection.Metadata;
-
-namespace Bankkonto
+﻿namespace Bankkonto
 {
     public class BankAccount
     {
-        private int Balance {  get; set; }
-        public BankAccount() 
-        { 
-            Balance = 0;    
-        }
+        // private field
+        private int _balance;
 
-        public BankAccount(int balance) 
-        { 
-            Balance = balance;
-        }
-
-        public void Deposit(int amount)
+        // constructor
+        public BankAccount()
         {
-            Balance += amount;
+            _balance = 0;
+        }
+        public int Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
         }
 
-        public void showBalance()
+        // deposit money
+        public void Deposit()
         {
-            Console.WriteLine($"Your balance {Balance}");
+            Console.Write("How much do you want to deposit?: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            if (userInput < 0)
+            {
+                Console.WriteLine("\nBalance can't be negative");
+                Balance = 0;
+                
+                Console.WriteLine("Please try again");
+            }
+
+            else
+            {
+                Balance += userInput;
+            }
+        }
+
+        // show balance
+        public void ShowBalance()
+        {
+            Console.WriteLine($"\nYour balance is {Balance}");
         }
     }
 }
