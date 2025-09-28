7c2bda992893ef4351f270351f46393c65ada3e3
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-28 12:01:48 +0200

    made a simple chatbot that generates a random answer when you ask a question

---
+++ Sunday/OOP/AI Chatbot Imitator/AI Chatbot/Bot.cs
@@ -0,0 +1,33 @@
+﻿namespace AI_Chatbot
+{
+    // make a public class called Bot
+    public class Bot
+    {
+        // here i make a method that accepts user input
+        public void userInput()
+        {
+            Console.WriteLine("Ask me anything!");
+            string question = Console.ReadLine();
+        }
+        
+        // make a method for the bot to generate a random answer from an array
+        public void generateAnswer()
+        {
+            string[] answers =
+            {
+                "40 celcius, it's very hot!",
+                "Haha, that's funny",
+                "I like pizza",
+                "Cats are funny"
+            };
+
+            // here we are using the random class to generate random answers from the array
+
+            Random random = new Random();
+
+            int index = random.Next(answers.Length);
+            Console.WriteLine($"Chatbot says: " + answers[index]);
+
+        }
+    }
+}
---
+++ Sunday/OOP/AI Chatbot Imitator/AI Chatbot/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AI_Chatbot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            bot.userInput();
+            bot.generateAnswer();
+        }
+    }
+}
