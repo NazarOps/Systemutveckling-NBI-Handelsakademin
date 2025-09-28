21092f7fd300d5898a6056e86d93da779860fbea
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-28 12:51:00 +0200

    made a simple calculator that adds, subtracts, divides and multiplies numbers

---
+++ Sunday/OOP/Calculator/Addition.cs
@@ -0,0 +1,26 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        public int addition;
+        public int multiplication;
+        public int division;
+        public int subtraction;
+
+
+        public void Calculate()
+        {
+            Console.WriteLine("Hello, what would you like to calculate today?\n");
+            Console.Write("\nUser: ");
+            string numberOne = Console.ReadLine();
+            Console.Write("User: ");
+            string numberTwo = Console.ReadLine();
+            
+            Console.WriteLine($"\nAddition: {addition = Convert.ToInt32(numberOne) + Convert.ToInt32(numberTwo)}");
+            Console.WriteLine($"Subtraction: {subtraction = Convert.ToInt32(numberOne) - Convert.ToInt32(numberTwo)}");
+            Console.WriteLine($"Division: {division = Convert.ToInt32(numberOne) / Convert.ToInt32(numberTwo)}");
+            Console.WriteLine($"Multiplication: { multiplication = Convert.ToInt32(numberOne) * Convert.ToInt32(numberTwo)}");
+        }
+
+    }
+}   
