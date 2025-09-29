58199f5deeeacd48df90d88eb5f594b084bf12c0
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-29 15:13:08 +0200

    work in progress, need to fix loop

---
+++ Monday/OOP/NumberGuessingExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace NumberGuessingExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            user Userinput = new user();
+            Userinput.StartGame();
+            Userinput.UserGuess();
+
+            Random random = new Random();
+            int Target = random.Next(1, 100);
+
+
+        }
+    }
+}
---
+++ Monday/OOP/NumberGuessingExercise/userinput.cs
@@ -0,0 +1,34 @@
+﻿namespace NumberGuessingExercise
+{
+    public class user
+    {
+        public int Target;
+
+        public void StartGame()
+        {
+            Console.WriteLine("Guess a number");
+        }
+
+        public void UserGuess()
+        {
+
+            int User = Convert.ToInt32(Console.ReadLine());
+
+            while (User != Target)
+            {
+                if (User < Target)
+                {
+                    Console.WriteLine("Too low, try again!");
+                }
+                else if (User > Target)
+                {
+                    Console.WriteLine("Too high, try again!");
+                }
+                else (User = Target);
+                {
+                    Console.WriteLine("Congratulations, you guessed right!");
+                }
+            } 
+        }
+    }
+}
