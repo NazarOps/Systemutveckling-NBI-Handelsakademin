7c1665852fd9e44ee50471e25845fa1066e0a168
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-30 11:53:36 +0200

    Thermometer exercise with some if statements

---
+++ Tuesday/OOP/Thermometer exercise condition statements/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Thermometer_exercise_condition_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Thermometer thermometer = new Thermometer();
+            thermometer.InputTemperature();
+            thermometer.CheckTemperature();
+        }
+    }
+}
---
+++ Tuesday/OOP/Thermometer exercise condition statements/Thermometer.cs
@@ -0,0 +1,41 @@
+﻿namespace Thermometer_exercise_condition_statements
+{
+    internal class Thermometer
+    {
+        public int Temperature;
+
+
+        public void InputTemperature()
+        {
+            Console.WriteLine("What's the temperature today?");
+            Temperature = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+        public void CheckTemperature()
+        {
+            if (Temperature >= -14 && Temperature <= 5)
+            {
+                Console.WriteLine("It's kind of cold in here...");
+            }
+
+            else if (Temperature <= -15)
+            {
+                Console.WriteLine("Wow, it's freezing in here!");
+            }
+
+            else if (Temperature >= 20)
+            {
+                Console.WriteLine("It's quite hot here!");
+            }
+
+            else
+            {
+                Console.WriteLine("The temperature is nice and comfortable");
+            }
+        }
+    }
+
+
+    
+}
