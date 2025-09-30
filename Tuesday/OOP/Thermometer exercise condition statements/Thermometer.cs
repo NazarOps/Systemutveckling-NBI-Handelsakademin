75e6a5663e4073c54ccf5d3c00cb9039f87f0901
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-30 12:00:15 +0200

    added some comments

--- Tuesday/OOP/Thermometer exercise condition statements/Thermometer.cs
+++ Tuesday/OOP/Thermometer exercise condition statements/Thermometer.cs
@@ -7,6 +7,7 @@
 
         public void InputTemperature()
         {
+            // Takes in user input as a string then converts it to an integer
             Console.WriteLine("What's the temperature today?");
             Temperature = Convert.ToInt32(Console.ReadLine());
         }
@@ -14,6 +15,7 @@
 
         public void CheckTemperature()
         {
+            // Here I set a range of values for the if statement, both conditions have to be met in order to execute
             if (Temperature >= -14 && Temperature <= 5)
             {
                 Console.WriteLine("It's kind of cold in here...");
