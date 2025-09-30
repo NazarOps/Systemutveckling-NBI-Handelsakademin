9bbbf4ced771cdf95fa70a4957d89a8d7537d4c4
Nazar Kokovkin <lilnz4@hotmail.com>
2025-09-30 16:23:30 +0200

    made a simple car insurance valid checker using if-else statements in C#.

---
+++ Tuesday/OOP/FordonsbesiktningExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FordonsbesiktningExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle();
+            vehicle.Inspection();
+            vehicle.CheckInspection();
+        }
+    }
+}
---
+++ Tuesday/OOP/FordonsbesiktningExercise/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿namespace FordonsbesiktningExercise
+{
+    public class Vehicle
+    {
+        public int VehicleAge;
+        public string HasInsurance;
+
+
+        public void Inspection()
+        {
+            Console.WriteLine("Do you have valid insurance?");
+            HasInsurance = Console.ReadLine().ToLower(); // convert all user inputs to lowercase so that the code doesnt break
+            Console.WriteLine("How old is your car?");
+            VehicleAge = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void CheckInspection()
+        {
+            if (VehicleAge < 5 && HasInsurance == "yes") 
+            {
+                Console.WriteLine("Your car is cleared of inspection");
+            }
+
+            else if (VehicleAge > 5 && HasInsurance == "no")
+            {
+                Console.WriteLine("Your car has not been cleared");
+            }
+
+            else
+            {
+                Console.WriteLine("Your insurance must be supplemented");
+                Console.WriteLine("Your car has not been cleared");
+            }
+        }
+    }
+}
