a353d1d3419892d4c481fa57eb55617302518865
Nazar Kokovkin <lilnz4@hotmail.com>
2025-10-01 20:08:58 +0200

    reduced code, more clean and wrapped inside a while loop instead

--- Tuesday/OOP/BankAccountSimulation/MenuHelper.cs
+++ Tuesday/OOP/BankAccountSimulation/MenuHelper.cs
@@ -10,92 +10,87 @@ namespace BankAccountSimulation
         public static int DepositMoney;
         public static int Withdraw;
 
-        // call the method ShowMenu which displays the menu and the 4 alternatives you can do
-        public static void ShowMenu()
-        {
-            Console.WriteLine("Hello! What would you like to do today?");
-            Console.WriteLine("1) Deposit");
-            Console.WriteLine("2) Withdraw");
-            Console.WriteLine("3) Show Balance");
-            Console.WriteLine("4) Log Out");
-        }
-
-        // this is a separate method for userinteraction with the console
+        // calls the method UserInteraction
         public static void UserInteraction()
         {
-            Console.Write("\nUser: ");
-            UserInput = Convert.ToInt32(Console.ReadLine()); // takes in user input and converts it to integer
-
-            // below here are the 4 alternatives the user can choose from, if user pressed 1, it will display deposit, if 2 then withdraw etc
-            // if user inputs a number that's not from 1-4 then the application will close
-
-            if (UserInput == 1)
+            bool isRunning = true;  // here i made so that the menu loops with a bool statement, while the statement is true
+            while (isRunning)       // the menu will keep running
             {
-                Console.Write("How much would you like to deposit?: ");
-                DepositMoney = Convert.ToInt32(Console.ReadLine());
-                Console.Clear(); // added console.clear to keep the console clean and not confuse the user with old inputs
-                
+                Console.WriteLine("Hello! What would you like to do today?");
+                Console.WriteLine("1) Deposit");
+                Console.WriteLine("2) Withdraw");
+                Console.WriteLine("3) Show Balance");
+                Console.WriteLine("4) Log Out");
 
-                if (DepositMoney <= 0)
+                Console.Write("\nUser: ");
+                UserInput = Convert.ToInt32(Console.ReadLine()); // takes in user input and converts it to integer
+
+                // below here are the 4 alternatives the user can choose from, if user pressed 1, it will display deposit, if 2 then withdraw etc
+                // if user inputs a number that's not from 1-4 then the application will close
+
+                if (UserInput == 1)
                 {
-                    Console.WriteLine("Balance can't be negative!");
-                    Console.WriteLine("No money has been deposited\n");
-                    ShowMenu();
-                    UserInteraction();
+                    Console.Write("How much would you like to deposit?: ");
+                    DepositMoney = Convert.ToInt32(Console.ReadLine());
+                    //Console.Clear(); // added console.clear to keep the console clean and not confuse the user with old inputs
+
+                    if (DepositMoney <= 0)
+                    {
+                        Console.WriteLine("Balance can't be negative!");
+                        Console.WriteLine("No money has been deposited\n");
+
+                        Thread.Sleep(900);
+                        Console.Clear();
+                    }
+
+                    else if (DepositMoney > 0)
+                    {
+                        Balance += DepositMoney; // the amount that user inputs will be added, if it was minus instead then it would be withdrawing
+                        Console.WriteLine($"Deposit successful, new balance: {Balance}\n");
+
+                        Thread.Sleep(900); // added thread.sleep for a timeout effect
+                        Console.Clear();
+                    }
+                    
+                    
                 }
 
-                else if (DepositMoney > 0)
+                if (UserInput == 2)
                 {
-                    Balance += DepositMoney; // the amount that user inputs will be added, if it was minus instead then it would be withdrawing
-                    Console.WriteLine($"Deposit successful, new balance: {Balance}\n");
-                    ShowMenu();
-                    UserInteraction();
-                }
-            }
+                    Console.Write("How much would you like to withdraw: ");
+                    Withdraw = Convert.ToInt32(Console.ReadLine());
 
-            if (UserInput == 2)
-            {
-                Console.Write("How much would you like to withdraw: ");
-                Withdraw = Convert.ToInt32(Console.ReadLine());
-                Console.Clear();
+                    if (Withdraw <= Balance)
+                    {
+                        Balance -= Withdraw;
+                        Console.WriteLine($"Successfully withdrawn: {Withdraw}");
+                        Console.WriteLine($"New balance: {Balance}\n");
+                        Thread.Sleep(1200);
+                        Console.Clear();
+                    }
 
-
-                if (Withdraw <= Balance)
-                {
-                    Balance -= Withdraw;
-                    Console.WriteLine($"Successfully withdrawn: {Withdraw}");
-                    Console.WriteLine($"New balance: {Balance}\n");
-                }
-
-                else if (Withdraw > Balance)
-                {
-                    Console.WriteLine("You can't withdraw more than you have!");
-                    Console.WriteLine($"Your balance: {Balance}");
+                    else if (Withdraw > Balance)
+                    {
+                        Console.WriteLine("You can't withdraw more than you have!");
+                        Console.WriteLine($"Your balance: {Balance}");
+                        Thread.Sleep(1200);
+                        Console.Clear();
+                    }
                 }
 
-                ShowMenu();
-                UserInteraction();
+                if (UserInput == 3)
+                {
+                    Console.WriteLine($"Your balance is {Balance}\n");
+                    Thread.Sleep(1200);
+                    Console.Clear();
+                }
 
-            }
-
-            if (UserInput == 3)
-            {
-                Console.WriteLine($"Your balance is {Balance}\n");
-                ShowMenu();
-                UserInteraction();
-            }
-
-            else if (UserInput == 4)
-            {
-                Console.WriteLine("Goodbye, have a nice day!");
-                return;
-            }
-
-            else
-            {
-                Console.WriteLine("You have inputted the wrong keys");
-                Console.WriteLine("This application will end");
-                Console.WriteLine("Goodbye");
+                if (UserInput == 4)
+                {
+                    Console.WriteLine("Goodbye, have a nice day!");
+                    Thread.Sleep(1200);
+                    break;
+                }
             }
         }
     }
