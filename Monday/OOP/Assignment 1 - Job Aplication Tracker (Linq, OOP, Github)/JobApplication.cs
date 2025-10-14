4abd582578c25a5743220a99098a998b2c875dfc
Nazar Kokovkin <lilnz4@hotmail.com>
2025-10-14 11:44:03 +0200

    WIP

---
+++ Monday/OOP/Assignment 1 - Job Aplication Tracker (Linq, OOP, Github)/JobApplication.cs
@@ -0,0 +1,29 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1___Job_Aplication_Tracker__Linq__OOP__Github_
+{
+    public class JobApplication
+    {
+        public string CompanyName;
+        public string PositionTitle;
+        public Status Status;
+        public DateTime ApplicationDate;
+        public DateTime? ResponseDate;
+        public int SalaryExpectation;
+
+        public void GetDaysSinceApplied()
+        {
+
+        }
+
+        public void GetSummary()
+        {
+
+        }
+    }
+}
--- Monday/OOP/Assignment 1 - Job Aplication Tracker (Linq, OOP, Github)/Program.cs
+++ Monday/OOP/Assignment 1 - Job Aplication Tracker (Linq, OOP, Github)/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool MenuLoop = true;
+
+            while (MenuLoop)
+            {
+                Console.WriteLine("Job Application Tracker\n");
+                Console.WriteLine("1) Apply for a new job");
+                Console.WriteLine("2) Show all applications");
+                Console.WriteLine("3) Filter by status");
+                Console.WriteLine("4) Sort by date");
+                Console.WriteLine("5) Show statistics");
+                Console.WriteLine("Type 6 to quit the application");
+
+
+                Console.Write("\nuser: ");
+                int userinput = Convert.ToInt32(Console.ReadLine());
+
+                switch (userinput)
+                {
+                    case 1:
+                        Console.WriteLine("hello");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("goodbye");
+                        break;
+                }
+
+                if (userinput == 10)
+                {
+                    break;
+                }
+            }
         }
     }
 }
