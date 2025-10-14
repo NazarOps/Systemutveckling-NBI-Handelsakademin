bc56cd03d7411f810fd33437986d2a57ced450ba
Nazar Kokovkin <lilnz4@hotmail.com>
2025-10-14 15:00:38 +0200

    fixed jobapplication list, now properly adds jobs to the list and displays them with GetSummary()

---
+++ Tuesday/Job Application Tracker/JobApplication.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job_Application_Tracker
+{
+    public class JobApplication
+    {
+        public string CompanyName { get; set; }
+        public string PositionTitle { get; set; }
+        public DateTime ApplicationDate { get; set; }
+        public DateTime? ResponseDate { get; set; }
+        public int SalaryExpectation { get; set; }
+
+        public Status ApplicationStatus { get; set; }
+
+        public int DaysSinceApplied => (DateTime.Now - ApplicationDate).Days;
+
+        
+
+        List<JobApplication> jobApplications = new List<JobApplication>();
+
+        public enum Status
+        {
+            Applied,
+            Interview,
+            Offer,
+            Rejected
+        }
+
+        public void Apply()
+        {
+            Console.WriteLine("What's the company called?");
+            Console.Write("\nUser: ");
+            string NameOfCompany = Console.ReadLine();
+
+            Console.WriteLine("What's the position title?");
+            Console.Write("User: ");
+            string TitleOfPosition = Console.ReadLine();
+
+            JobApplication jobapplied = new JobApplication
+            {
+                CompanyName = NameOfCompany,
+                PositionTitle = TitleOfPosition,
+                ApplicationStatus = Status.Applied,
+                ApplicationDate = DateTime.Now
+            };
+                
+            jobApplications.Add(jobapplied);
+        }
+
+        public void GetDaysSinceApplied()
+        {
+
+        }
+
+        public void GetSummary()
+        {
+            foreach (JobApplication j in jobApplications)
+            {
+                Console.WriteLine($"\nName: {j.CompanyName}");
+                Console.WriteLine($"Title: {j.PositionTitle}");
+                Console.WriteLine($"Status: {j.ApplicationStatus}");
+                Console.WriteLine($"Date: {j.ApplicationDate}");
+            }
+        }
+    }
+}
---
+++ Tuesday/Job Application Tracker/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Job_Application_Tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool MenuLoop = true;
+
+            
+            JobApplication jobApplication = new JobApplication();
+            
+            while (MenuLoop)
+            {
+                Console.WriteLine("Job Application Tracker\n");
+                Console.WriteLine("1) Apply for a new job");
+                Console.WriteLine("2) Show all applications");
+                Console.WriteLine("3) Filter by status");
+                Console.WriteLine("4) Sort by date");
+                Console.WriteLine("5) Show statistics");
+                Console.WriteLine("\nType 6 to quit the application");
+
+                Console.Write("\nuser: ");
+                int userinput = Convert.ToInt32(Console.ReadLine());
+
+                switch (userinput)
+                {
+                    case 1:
+                        Console.Clear();
+                        jobApplication.Apply();
+                        Thread.Sleep(500);
+                        Console.Clear();
+                        break;
+                        
+                    case 2:
+                        Console.WriteLine("Show all");
+                        jobApplication.GetSummary();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Filter by status");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Show by date");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Show statistics");
+                        break;
+                }
+
+                if (userinput == 6)
+                {
+                    break;
+                }
+
+                
+            }
+        }
+    }
+}
