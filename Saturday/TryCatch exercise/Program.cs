003c105a6671a17b212fbaf33fb584e0b96faf4b
Nazar Kokovkin <lilnz4@hotmail.com>
2025-10-25 23:22:50 +0200

    try catch exercise done

---
+++ Saturday/TryCatch exercise/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TryCatch_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try    //try block checks for wrong input
+                {
+                    Console.Write("User: ");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("input is a number");
+                    Console.WriteLine("Closing application...");
+                    break;
+                }
+
+
+
+                catch (FormatException) //catch catches the wrong input and throws and error
+                {
+                    Console.WriteLine("You must type in a number!");
+                }
+            }
+        }
+    }
+}
