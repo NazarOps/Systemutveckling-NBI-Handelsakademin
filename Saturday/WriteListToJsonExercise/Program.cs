44f8f2f7b0f2656739606a7aa79d58e4951e90ed
Nazar Kokovkin <lilnz4@hotmail.com>
2025-10-25 23:49:46 +0200

    read list json exercise done

---
+++ Saturday/WriteListToJsonExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace WriteListToJsonExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> myList = new List<string>();
+            //myList.Add("Item 1");
+            //myList.Add("Item 2");
+            //myList.Add("Item 3");
+
+
+            //string saveJson = JsonSerializer.Serialize(myList);
+
+            //File.WriteAllText("myList.json", saveJson);
+
+            //Console.WriteLine("List saved to json");
+
+            string FileToRead = "myList.json";
+            string readJson = File.ReadAllText(FileToRead);
+
+            Console.WriteLine(readJson);
+        }
+    }
+}
