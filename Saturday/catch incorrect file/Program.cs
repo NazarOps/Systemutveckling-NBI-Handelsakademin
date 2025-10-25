ceb8aac399e58fa25fa675304c26ca693b723342
Nazar Kokovkin <lilnz4@hotmail.com>
2025-10-25 23:38:17 +0200

    exercise done

---
+++ Saturday/catch incorrect file/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace catch_incorrect_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string file = "FileDoesNotExist.json";
+                string readjson = File.ReadAllText("file");
+
+                Console.WriteLine("File Content: ");
+                Console.WriteLine(readjson);
+            }
+
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("File does not exist!!!!");
+            }
+
+        }
+    }
+}
