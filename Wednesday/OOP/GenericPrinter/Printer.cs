e367ee7f4e03438006ee6efbc08a9243e63af10f
Nazar Kokovkin <lilnz4@hotmail.com>
2025-10-30 12:40:10 +0100

    generic printer

--- Wednesday/OOP/GenericPrinter/Printer.cs
+++ Wednesday/OOP/GenericPrinter/Printer.cs
@@ -8,15 +8,29 @@ namespace GenericPrinter
 {
     public class Printer
     {
-        List<string> listofstrings = new List<string>();
-        List<int> listofints = new List<int>();
-        List<double> listofdoubles = new List<double>();
+        public List<string> listofstrings = new List<string>();
+        public List<int> listofints = new List<int>();
+        public List<double> listofdoubles = new List<double>();
 
+        
 
+        public void AddItems()
+        {
+            listofstrings.Add("string 1");
+            listofstrings.Add("string 2");
+            listofstrings.Add("string 3");
 
+            listofints.Add(1);
+            listofints.Add(2);
+            listofints.Add(3);
 
+            listofdoubles.Add(1.5);
+            listofdoubles.Add(1.3);
+            listofdoubles.Add(1.93);
+        }
 
-        public static void PrintItems<T>(List<T> items)
+        // static can only be called inside the class
+        public void PrintItems<T>(List<T> items)
         {
             foreach (var thing in items)
             {
--- Wednesday/OOP/GenericPrinter/Program.cs
+++ Wednesday/OOP/GenericPrinter/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace GenericPrinter
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GenericPrinter
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //           Skapa en generisk metod PrintItems<T>(List < T > items) som skriver ut alla element oavsett typ.
+            //💡 Testa med listor av string, int och double.
+
+
+            Printer printer = new Printer();
+            printer.AddItems();
+
+            printer.PrintItems(printer.listofstrings);
+            printer.PrintItems(printer.listofints);
+            printer.PrintItems(printer.listofdoubles);
         }
     }
 }
